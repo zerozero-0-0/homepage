10f6ef700bd67ec1514c9ecd12994ab532c17a04
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2024-12-23 10:30:55 +0900

    Footerを作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import  Header  from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      created by zerozero
+    </div>
+  )
+}
+
+export default Footer

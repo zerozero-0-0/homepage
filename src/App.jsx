501a79593c9172289ad2b2117689b660ed6ed0d2
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2024-12-28 18:49:05 +0900

    routerの実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
-import  Header  from "./components/Header/Header";
+import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
+
+import { Routes, Route, Link } from "react-router";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <About />
+      <Link to="/">Home</Link>
+      <Link to="/hoge">hoge</Link>
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/hoge" element={<p>hello world</p>} />
+      </Routes>
       <Footer />
     </div>
   );

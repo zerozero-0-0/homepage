63a3d8e6db7ce9a22f30a92fbb1bd2ede9769c3e
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2025-01-03 16:01:28 +0900

    見出しをある程度作成

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
 
-import './Contact.css'
+import "./Contact.css";
 const Contact = () => {
   return (
-    <div className='contact'>Coming Soon...</div>
-  )
-}
+    <div className="contact">
+      <div className="heading">
+      <h2 className="back_font"> Contact</h2>
+      <h2 className="front_font"> お問い合わせ</h2>
+      </div>
+      Coming Soon...
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;

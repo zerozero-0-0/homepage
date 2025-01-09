a0cd044c78e4174c5643a475155afb67de3476cd
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2025-01-09 13:16:26 +0900

    TabKeyUIをヘッダー(menuTab以外)に実装

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import Tab from "../Tab/Tab";
 // import CSS
@@ -11,8 +11,40 @@ import { BsTwitterX } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
 
 const Header = () => {
+  // TabKeyに対するUI
+  const headerRef = useRef(null);
+  const focusableElementsRef = useRef([]);
+
+  const handleKeyDown = (e) => {
+    if(e.key === "Tab") {
+      const focusableElements = focusableElementsRef.current;
+      const currentIndex = focusableElements.indexOf(document.activeElement);
+      if(e.shiftKey) {
+        // shift + Tab
+        const prevIndex = (currentIndex - 1 + focusableElements.length) % focusableElements.length;
+        focusableElements[prevIndex].focus();
+      } else {
+        // Tab
+        const nextIndex = (currentIndex + 1) % focusableElements.length;
+        focusableElements[nextIndex].focus();
+      }
+      e.preventDefault();
+    }
+  };
+
+  useEffect(() => {
+    const focusableElements = headerRef.current.querySelectorAll(
+      'a'
+    );
+    focusableElementsRef.current = Array.from(focusableElements);
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    }
+  }, []);
+
   return (
-    <div className="header">
+    <div className="header" ref={headerRef}>
       <div className="tab">
         <Tab />
       </div>
